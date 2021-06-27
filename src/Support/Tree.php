ca27c1347138a759c21aab0b5c6776ce86992446
Harold Kim <groovey@users.noreply.github.com>
2021-06-27 18:51:33 +0800

    Rework on the tree

--- src/Support/Tree.php
+++ src/Support/Tree.php
@@ -19,7 +19,7 @@ class Tree
     /**
      * Sort base on order
      */
-    public function sort(array $datas)
+    private function sort(array $datas)
     {
         usort($datas, function ($a, $b) {
             return $a['order'] <=> $b['order'] ;
@@ -29,9 +29,9 @@ class Tree
     }
 
     /**
-     * reOrder base on parent_id
+     * rearrange datas base on parent_id
      */
-    public function reOrder($array, $parent=null, $indent='')
+    private function reOrder($array, $parent=null, $indent='')
     {
         $return = [];
         foreach ($array as $key => $val) {
@@ -41,14 +41,6 @@ class Tree
             }
         }
         return $return;
-    }
-
-    /**
-     * Returns data in array form
-     */
-    public function toArray()
-    {
-        return $this->datas;
     }
 
     /**
@@ -67,6 +59,51 @@ class Tree
     public function toObject()
     {
         return json_decode(json_encode($this->datas));
+    }
+
+    /**
+     * Returns data in array form
+     */
+    public function toArray()
+    {
+        return $this->datas;
+    }
+
+    /**
+     * To a multi dimensional array
+     */
+    public function toNestedArray()
+    {
+        $datas      = $this->datas;
+        $categories = [];
+
+        // make associative array of $category with respective id as key
+        foreach ($datas as $data) {
+            $categories[$data['id']]= $data;
+        }
+
+        // fill in 'subcategories' while maintaining the references using '&' operator
+        foreach ($categories as $key => &$data) {
+            if ($data['parent_id'] != null) {
+                $categories[$data['parent_id']]['subcategories'][$data['slug']] = &$data;
+            }
+        }
+
+        // Cleanup duplicate occurences
+        foreach ($categories as $category) {
+
+            // Removed unwanted duplicates
+            if ($category['parent_id'] != null) {
+                unset($categories[$category['id']]);
+
+            // Replace the array key to the slug name instead of the id
+            } else {
+                $categories[$category['slug']] = $categories[$category['id']];
+                unset($categories[$category['id']]);
+            }
+        }
+
+        return $categories;
     }
 
     /**
